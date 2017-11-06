f89aeef76ab2cb9a73794ac8b6ecb4954f64ffcc
Per Abich <per.abich@ig.com>
2017-11-06 16:21:19 +0000

    Added example command

---
+++ cmd/haproxy-cfg2json/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/IG-Group/haproxyconfigparser"
+	"log"
+	"os"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		log.Fatal("You need to specify the haproxy.cfg file.")
+	}
+	filename := os.Args[1]
+
+	services, err := haproxyconfigparser.ParseFromFile(filename)
+	if err != nil {
+		log.Fatalf("Failed to parse the config: %s", err)
+	}
+
+	bytes, err := json.MarshalIndent(services, "", "  ")
+	if err != nil {
+		log.Fatalf("Failed to translate to JSON")
+	}
+
+	fmt.Println(string(bytes))
+}
